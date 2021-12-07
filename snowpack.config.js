34facf196bf12e6c5981bada591198b1fea9b90b
Markus Tacker <m@coderbyheart.com>
2021-12-07 14:48:41 +0100

    feat: initial commit of app shell

---
+++ snowpack.config.js
@@ -0,0 +1,24 @@
+process.env.SNOWPACK_PUBLIC_VERSION = process.env.VERSION || Date.now()
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+	mount: {
+		public: '/',
+		src: '/_dist_',
+	},
+	plugins: ['@snowpack/plugin-react-refresh', '@snowpack/plugin-typescript'],
+	packageOptions: {
+		installTypes: true,
+		env: {
+			SNOWPACK_PUBLIC_VERSION: true,
+		},
+	},
+	buildOptions: {
+		...(process.env.SNOWPACK_PUBLIC_DEVICE_UI_BASE_URL !== undefined && {
+			baseUrl: `${process.env.SNOWPACK_PUBLIC_DEVICE_UI_BASE_URL.replace(
+				/\/+$/,
+				'',
+			)}/`,
+		}),
+	},
+}
